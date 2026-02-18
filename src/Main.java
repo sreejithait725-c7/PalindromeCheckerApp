64fa5b8ec07f94e06115a568682e9faa6ab7d07a
Srivijnatha <srivi.2125@gmail.com>
2026-02-18 14:49:14 +0530

    welcome message module

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("   Welcome to PalindromeChecker App    ");
+        System.out.println("Application Version: 1.0");
+        System.out.println("Developed for: Palindrome Validation");
+        System.out.println("Ready to check palindromes...");
+    }
+}
